e2eb4b398ab9827a2afd88307caef7d221b6a186
ChristopherHX <christopher.homberger@web.de>
2022-07-08 12:18:03 +0200

    tweak it

--- src/libANGLE/renderer/vulkan/mac/WindowSurfaceVkMac.mm
+++ src/libANGLE/renderer/vulkan/mac/WindowSurfaceVkMac.mm
@@ -25,33 +25,20 @@ WindowSurfaceVkMac::WindowSurfaceVkMac(const egl::SurfaceState &surfaceState,
 
 WindowSurfaceVkMac::~WindowSurfaceVkMac()
 {
-    [mMetalLayer release];
 }
 
 angle::Result WindowSurfaceVkMac::createSurfaceVk(vk::Context *context, gl::Extents *extentsOut)
     API_AVAILABLE(macosx(10.11))
 {
 
-    CALayer *layer = reinterpret_cast<CALayer *>(mNativeWindowType);
+    mMetalLayer *layer = reinterpret_cast<CAMetalLayer *>(mNativeWindowType);
 
-    mMetalLayer        = [[CAMetalLayer alloc] init];
-    mMetalLayer.frame  = CGRectMake(0, 0, layer.frame.size.width, layer.frame.size.height);
-    mMetalLayer.drawableSize =
-        CGSizeMake(mMetalLayer.bounds.size.width * mMetalLayer.contentsScale,
-                   mMetalLayer.bounds.size.height * mMetalLayer.contentsScale);
-    mMetalLayer.framebufferOnly  = NO;
-    mMetalLayer.autoresizingMask = kCALayerWidthSizable | kCALayerHeightSizable;
-    mMetalLayer.contentsScale    = layer.contentsScale;
-
-    [layer addSublayer:mMetalLayer];
-
-    VkMetalSurfaceCreateInfoEXT createInfo = {};
-    createInfo.sType                       = VK_STRUCTURE_TYPE_METAL_SURFACE_CREATE_INFO_EXT;
-    createInfo.flags                       = 0;
-    createInfo.pNext                       = nullptr;
-    createInfo.pLayer                      = mMetalLayer;
+    VkMetalSurfaceCreateInfoEXT sci = {};
+    memset(&sci, 0, sizeof(sci));
+    sci.sType = VK_STRUCTURE_TYPE_METAL_SURFACE_CREATE_INFO_EXT;
+    sci.pLayer = window->ns.layer;
     ANGLE_VK_TRY(context, vkCreateMetalSurfaceEXT(context->getRenderer()->getInstance(),
-                                                  &createInfo, nullptr, &mSurface));
+                                                  &sci, nullptr, &mSurface));
 
     return getCurrentWindowSize(context, extentsOut);
 }
